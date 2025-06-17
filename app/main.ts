a6fae98f5420762dfc39357837786e7de9791325
JJooaa <leitola.joa@gmail.com>
2025-06-17 17:32:46 +0300

    echo route

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,13 @@
 import * as net from "node:net";
 
-const allowedRoutes = ["/"];
-
-function response(status: string) {
-  return `HTTP/1.1 ${status}\r\n\r\n`;
+function response(status: string, body?: string) {
+  return (
+    `HTTP/1.1 ${status}\r\n` +
+    "Content-Type: text/plain\r\n" +
+    `Content-Length: ${body?.length ?? 0}\r\n` +
+    "\r\n" + // end of headers
+    `${body ?? ""}`
+  );
 }
 
 function extractRouteFromRequest(request: string) {
@@ -22,14 +26,18 @@ const server = net.createServer((socket: net.Socket) => {
     request += chunk.toString();
     const route = extractRouteFromRequest(request);
 
-    if (!allowedRoutes.includes(route)) {
-      socket.write(response("404 Not Found"));
-      socket.end();
-    } else {
+    if (route === "/") {
       socket.write(response("200 OK"));
-      socket.end();
+      return socket.end();
     }
-    socket.end();
+    if (route.includes("/echo/")) {
+      const echoedValue = route.split("/echo/")[1]; // [ "", "echoed_value_here]" ]
+      socket.write(response("200 OK", echoedValue));
+      return socket.end();
+    } else {
+      socket.write(response("404 Not Found"));
+      return socket.end();
+    }
   });
 
   socket.on("close", () => {
