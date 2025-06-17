cba5d441ab7c389c2b919ebe205a86e21c27cdfe
JJooaa <leitola.joa@gmail.com>
2025-06-17 16:08:21 +0300

    extract url path

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,38 @@
-import * as net from "net";
+import * as net from "node:net";
 
-console.log("Logs from your program will appear here!");
+const allowedRoutes = ["/"];
 
-const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+function response(status: string) {
+  return `HTTP/1.1 ${status}\r\n\r\n`;
+}
+
+function extractRouteFromRequest(request: string) {
+  const requestArray = request.split("\r\n");
+  const path = requestArray[0].split(" ")[1];
+  return path;
+}
+
+console.log("Server running: Listening for events...");
+
+// Listens for *raw TCP sockets*.
+const server = net.createServer((socket: net.Socket) => {
+  let request = "";
+
+  socket.on("data", (chunk: Buffer) => {
+    request += chunk.toString();
+    const route = extractRouteFromRequest(request);
+
+    if (!allowedRoutes.includes(route)) {
+      socket.write(response("404 Not Found"));
+      socket.end();
+    } else {
+      socket.write(response("200 OK"));
+      socket.end();
+    }
+    socket.end();
+  });
+
   socket.on("close", () => {
-    console.log("socket ended");
     socket.end();
   });
 });
