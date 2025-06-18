cdd6fd6166ce80c0d0862e115b8803d4b4ab351b
JJooaa <leitola.joa@gmail.com>
2025-06-18 10:35:47 +0300

    return a file

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,22 @@
 import * as net from "node:net";
 
-function response(status: string, body?: string) {
+type ResponseArgs = {
+  status: string;
+  body?: string;
+  contentType?: string;
+};
+
+function response({
+  status,
+  body = "",
+  contentType = "text/plain",
+}: ResponseArgs) {
   return (
     `HTTP/1.1 ${status}\r\n` +
-    "Content-Type: text/plain\r\n" +
-    `Content-Length: ${body?.length ?? 0}\r\n` +
+    `Content-Type: ${contentType}\r\n` +
+    `Content-Length: ${body.length ?? 0}\r\n` +
     "\r\n" + // end of headers
-    `${body ?? ""}`
+    `${body}`
   );
 }
 
@@ -16,10 +26,21 @@ function extractRouteFromRequest(request: string) {
   return path;
 }
 
-function extractUserAgentFromRequest(request: string) {
+function getHeaderFromRequest(request: string, headerName: string) {
   const requestArray = request.split("\r\n");
-  const userAgent = requestArray.find((item) => item.includes("User-Agent"));
-  return userAgent;
+  const header = requestArray.find((item) => item.includes(headerName))?.trim();
+  const keyValue = header?.split(headerName);
+
+  return { key: keyValue?.[0], value: keyValue?.[1].trim() };
+}
+
+async function readFile(fileName: string) {
+  const dirIndex = process.argv.indexOf("--directory");
+  const directory = dirIndex !== -1 ? process.argv[dirIndex + 1] : "./files";
+  const file = Bun.file(`${directory}/${fileName}`);
+
+  if (await file.exists()) return await file.text();
+  return null;
 }
 
 console.log("Server running: Listening for events...");
@@ -28,35 +49,53 @@ console.log("Server running: Listening for events...");
 const server = net.createServer((socket: net.Socket) => {
   let request = "";
 
-  socket.on("data", (chunk: Buffer) => {
+  socket.on("data", async (chunk: Buffer) => {
     request += chunk.toString();
 
     const route = extractRouteFromRequest(request);
 
     if (route === "/") {
-      socket.write(response("200 OK"));
-      extractUserAgentFromRequest(request);
-
+      socket.write(response({ status: "200 OK" }));
       return socket.end();
     }
 
     if (route === "/user-agent") {
-      const userAgent = extractUserAgentFromRequest(request)
-        ?.split("User-Agent:")[1]
-        .trim();
-      console.log(userAgent);
-      socket.write(response("200 OK", userAgent));
+      const { value } = getHeaderFromRequest(request, "User-Agent:");
+      socket.write(response({ status: "200 OK", body: value }));
       return socket.end();
     }
 
     if (route.includes("/echo/")) {
       const echoedValue = route.split("/echo/")[1]; // [ "", "echoed_value_here]" ]
-      socket.write(response("200 OK", echoedValue));
+      socket.write(response({ status: "200 OK", body: echoedValue }));
       return socket.end();
     }
 
+    if (route.includes("/files/")) {
+      const fileName = route.split("/files/")[1];
+      const fileContents = await readFile(fileName);
+
+      if (fileContents) {
+        socket.write(
+          response({
+            status: "200 OK",
+            body: fileContents,
+            contentType: "application/octet-stream",
+          })
+        );
+        return socket.end();
+      } else {
+        socket.write(
+          response({
+            status: "404 Not Found",
+          })
+        );
+        return socket.end();
+      }
+    }
+
     // Unhandled routes:
-    socket.write(response("404 Not Found"));
+    socket.write(response({ status: "404 Not Found" }));
     return socket.end();
   });
 
